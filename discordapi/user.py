ef61864f4aceb29796416ea2b8f3dd49ab970aea
KokoseiJ <kokoseij@gmail.com>
2021-06-20 22:10:30 +0900

    Implemented User object

--- discordapi/user.py
+++ discordapi/user.py
@@ -1,5 +1,71 @@
+from .guild import Guild
+from .channel import Channel
 from .dictobject import DictObject
+
+import base64
+from io import BytesIO
+
+KEYLIST = ["id", "username", "discriminator", "avatar", "bot", "system",
+           "mfa_enabled", "locale", "verified", "email", "flags",
+           "premium_type", "public_flags"]
 
 
 class User(DictObject):
-    pass
+    def __init__(self, client, data):
+        super(User, self).__init__(data, KEYLIST)
+        self.client = client
+
+    def dm(self):
+        return self.client.user.create_dm(self)
+
+
+class BotUser(DictObject):
+    def modify_user(self, username=None, avatar=None):
+        if isinstance(avatar, str):
+            with open(avatar, "rb") as f:
+                avatar = f.read()
+        elif isinstance(avatar, BytesIO):
+            avatar = f.read()
+        avatar = base64.b64encode(avatar).decode()
+
+        postdata = {
+            "username": username,
+            "avatar": avatar
+        }
+
+        user = self.client.send_request(
+            "PATCH", "/users/@me", postdata
+        )
+
+        self.__init__(self.client, user)
+
+        return self
+
+    def leave_guild(self, guild):
+        if isinstance(guild, Guild):
+            guild = guild.id
+
+        self.client.send_request(
+            "DELETE", f"/users/@me/guilds/{guild}"
+        )
+
+    def create_dm(self, user):
+        if isinstance(user, User):
+            user = user.id
+
+        postdata = {
+            "recipient_id": user
+        }
+
+        channel = self.client.send_request(
+            "POST", "/users/@me/channels", postdata
+        )
+
+        return Channel(channel)
+
+    def get_connections(self):
+        connections = self.client.send_request(
+            "GET", "/users/@me/connections"
+        )
+
+        return connections
