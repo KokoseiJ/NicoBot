f1f3dd98313d4108deb93c1b3a17aaa037fa196b
KokoseiJ <kokoseij@gmail.com>
2021-09-17 12:04:33 -0700

    Created embed.py

---
+++ discordapi/embed.py
@@ -0,0 +1,65 @@
+#
+# NicoBot is Nicovideo Player bot for Discord, written from the scratch.
+# This file is part of NicoBot.
+#
+# Copyright (C) 2021 Wonjun Jung (KokoseiJ)
+#
+#    Nicobot is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from .const import EMPTY
+from .util import clear_postdata
+from .dictobject import DictObject
+
+__all__ = ["Embed"]
+
+KEYLIST = ["title", "type", "description", "url", "timestamp", "color",
+           "footer", "image", "thumbnail", "video", "provider", "author",
+           "fields"]
+
+
+class Embed(DictObject):
+    def __init__(self, *args, **kwargs):
+        kwargs.update(dict(zip(KEYLIST[:len(args)], args[:len(KEYLIST)])))
+        self.__init__(self, kwargs, KEYLIST)
+
+        if self.fields is not None and not isinstance(self.fields, list):
+            raise TypeError(f"fields should be list, not {type(self.fields)}")
+
+    def add_field(self, name, value, inline=False):
+        if self.fields is None:
+            self.fields = []
+        self.fields.append({
+            "name": name,
+            "value": value,
+            "inline": inline
+        })
+
+    def set_footer(self, text, icon=None):
+        self.footer = {
+            "text": text
+        }
+
+        if icon is not None:
+            self.footer.update({"icon_url": icon})
+
+    def set_author(self, name, url=None, icon=None):
+        self.author = {
+            "name": name
+        }
+
+        if url is not None:
+            self.author.update({"url": url})
+        if icon is not None:
+            self.author.update({"icon_url": icon})
